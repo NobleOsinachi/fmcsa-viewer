85ca7fd536d4fbbe17786d04ed97710751dcf929
Jeet Viramgama <jviramgama5@gmail.com>
2024-08-06 19:28:20 +0300

    feat: group by year,month,week & update table key
    Signed-off-by: Jeet Viramgama <jviramgama5@gmail.com>

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -7,3 +7,7 @@ export const getTimestamp = (date: string) => {
   const _date = new Date(date);
   return Math.floor(_date.getTime() / 1000);
 }
+
+export const capitalize = (str: string) => {
+  return str.replace(/_/g, ' ').replace(/\b\w/g, char => char.toUpperCase());
+};
--- src/components/PivotViewer.tsx
+++ src/components/PivotViewer.tsx
@@ -3,9 +3,39 @@ import React, { useEffect, useState } from 'react';
 import { Record } from '../common/types';
 import PivotTableUI from 'react-pivottable/PivotTableUI';
 import 'react-pivottable/pivottable.css';
+import { format, getYear, getISOWeek } from 'date-fns';
 import { columns } from '../common/constants';
+import { capitalize } from '../common/utils';
 
 const columnsToInclude = columns.map(col => col.key);
+
+const preprocessData = (records: Record[]): Record[] => {
+  return records.map((record: any) => {
+    const filteredRecord: any = {} as Record;
+
+    columnsToInclude.forEach(key => {
+      if (record[key]) {
+        const matchedLabel = columns.find((item) => item.key === key)?.label; // Get label and replace the key for better accessibility
+        filteredRecord[matchedLabel || capitalize(key)] = record[key];
+        if (key.includes('dt')) {
+          const date = new Date(record[key]);
+          filteredRecord[`${matchedLabel || capitalize(key)} Year`] = getYear(date);
+          filteredRecord[`${matchedLabel || capitalize(key)} Month`] = format(date, 'MMMM');
+          filteredRecord[`${matchedLabel || capitalize(key)} Week`] = getISOWeek(date);
+        }
+      }
+    });
+
+    return filteredRecord;
+  });
+};
+
+//* Note
+// preprocessData function is used to allow dates to be groupable by year/month/week
+// consider created_dt for example
+// 3 separate keys from it are created
+// date-fns i used to extract year, english month & ISO week of a particular date (Max 52 or 53 weeks in a year)
+// this is only applied to the function, if the key contains "dt" string which works for created_dt UNLIKE out_of_service_date (can be adjust with minor changes)
 
 const PivotViewer = ({ records }: { records: Record[] }) => {
   const [pivotState, setPivotState] = useState<any>({});
@@ -13,14 +43,7 @@ const PivotViewer = ({ records }: { records: Record[] }) => {
   const [dataKey, setDataKey] = useState(0);
 
   useEffect(() => {
-    const filteredData = records.map((record: any) =>
-      Object.keys(record)
-        .filter(key => columnsToInclude.includes(key))
-        .reduce((obj: any, key) => {
-          obj[key] = record[key];
-          return obj;
-        }, {} as Record),
-    );
+    const filteredData = preprocessData(records);
     setDataKey(prevKey => prevKey + 1); // Update the key to force re-render
     setFilteredRecords(filteredData);
     setPivotState((prevState: any) => ({ ...prevState, data: filteredData })); // Update data on re-render but keeps the selected filters
